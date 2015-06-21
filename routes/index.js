17e30e99ad59347072690860db2aec4cbabc0f4b
kimmy <iamnotwhoyouthink@gmail.com>
2015-06-21 14:55:49 +0100

    Server to update kittly split for the person that requested it.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var kittySplit = require('../lib/kittysplit')();
+var http = require('http');
+
+
+router.get('/:person', function (req, res) {
+	kittySplit.update(req.params.person);
+
+
+	res.send('happy');
+
+});
+module.exports = router;
